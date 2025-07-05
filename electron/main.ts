5ac7b3c6a956dc2bd6cb08be68d9266d5b438524
Jhonard Langcoyan <jhonardlangs@gmail.com>
2025-07-06 02:42:24 +0800

    add electron

---
+++ electron/main.ts
@@ -0,0 +1,23 @@
+import { app, BrowserWindow } from 'electron';
+import { join, dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+function createWindow() {
+    const win = new BrowserWindow({
+        width: 1024,
+        height: 768,
+        webPreferences: {
+            contextIsolation: true,
+            nodeIntegration: false,
+        }
+    });
+
+    win.loadFile(join(__dirname, '../dist/index.html'));
+    win.webContents.openDevTools(); //optional: Open DevTools for debugging
+
+}
+
+app.whenReady().then(createWindow);
